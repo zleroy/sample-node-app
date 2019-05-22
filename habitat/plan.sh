346a2e1f7d15f4a5273c2555a5bcc8f48f711d6a
zleroy <zleroy5@gmail.com>
2019-05-22 14:54:00 -0500

    Update plan.sh
    bump version

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -14,7 +14,7 @@
 
 pkg_name=sample-node-app
 pkg_origin=zleroy
-pkg_version="1.1.0"
+pkg_version="1.1.1"
 pkg_deps=(core/node)
 
 # Habitat provides you with a number of built-in "callbacks" to use
